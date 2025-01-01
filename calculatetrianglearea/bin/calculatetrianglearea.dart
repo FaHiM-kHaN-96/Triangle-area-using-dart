cd65e6f8dbb86fdaf0cf98634a84c08989b19039
Enamul Hakim Khan(Fahim) <123324337+FaHiM-kHaN-96@users.noreply.github.com>
2025-01-01 23:13:02 +0600

    Update calculatetrianglearea.dart

--- calculatetrianglearea/bin/calculatetrianglearea.dart
+++ calculatetrianglearea/bin/calculatetrianglearea.dart
@@ -5,16 +5,16 @@ void main() {
 }
 
 void triangle() {
-  // Get base and height from the user
+  
   stdout.write('Enter the base of the triangle: ');
   double base = double.parse(stdin.readLineSync()!);
 
   stdout.write('Enter the height of the triangle: ');
   double height = double.parse(stdin.readLineSync()!);
 
-  // Calculate the area
+  
   double area = 0.5 * base * height;
 
-  // Display the result
+  
   print('The area of the triangle is: $area');
 }
